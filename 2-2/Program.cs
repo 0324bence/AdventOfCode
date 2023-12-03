f9d60419a7e9400b12afcf32ac6eebfe9d823d2a
0324bence <bence20050324@gmail.com>
2023-12-03 21:37:08 +0100

    solve 2-2

--- 2-2/Program.cs
+++ 2-2/Program.cs
@@ -3,7 +3,37 @@
     internal class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            int output = 0;
+
+            string[] lines = File.ReadAllLines("input.txt");
+
+            foreach (string line in lines) {
+                string[] split1 = line.Split(": ");
+                int gameNum = int.Parse(split1[0].Split(' ')[1]);
+                string[] sets = split1[1].Trim().Split("; ");
+                Dictionary<string, int> amounts = new Dictionary<string, int>() {
+                    {"green", 0},
+                    {"blue", 0 },
+                    {"red", 0 }
+                };
+                foreach (string set in sets) {
+                    string[] moves = set.Split(", ");
+                    foreach (string move in moves) {
+                        string[] splitMove = move.Split(" ");
+                        int colorAmount = int.Parse(splitMove[0]);
+                        if (colorAmount > amounts[splitMove[1]]) {
+                            amounts[splitMove[1]] = colorAmount;
+                        }
+                    }
+                }
+                int power = 1;
+                foreach (int value in amounts.Values) {
+                    power *= value;
+                }
+                output += power;
+            }
+            Console.WriteLine(output);
         }
     }
 }
